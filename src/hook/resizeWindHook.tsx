c1ddbe29ebf985ebbfc37b0aaafa29385903997d
DzmitryMiankou <gmiankou@gmail.com>
2023-10-05 16:51:23 +0300

    r

--- src/hook/resizeWindHook.tsx
+++ src/hook/resizeWindHook.tsx
@@ -1,18 +1,17 @@
 import { useState, useEffect } from "react";
 
-export const useResize = () => {
+export const useResize = (): { width: number } => {
   const [width, setWidth] = useState<number>(window.innerWidth);
 
   useEffect(() => {
-    const handleResize = (event: any) => {
-      setWidth(event.target.innerWidth);
+    const handleResize = (event: UIEvent) => {
+      setWidth((event.target as Window).innerWidth);
     };
     window.addEventListener("resize", handleResize);
     return () => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
-
   return {
     width,
   };
