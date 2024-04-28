80328bd24e42e3c1f5c3129aeea3cb33adb97487
DzmitryMiankou <gmiankou@gmail.com>
2024-04-28 10:44:03 +0300

    random fC hook

--- src/hook/randomIntNum.tsx
+++ src/hook/randomIntNum.tsx
@@ -1,14 +1,23 @@
 type Prop<N extends number> = { min: N; max: N; length: N };
 type RandomProp<N extends number> = Pick<Prop<N>, "min" | "max">;
+type ResultNumbersType<N extends number> = {
+  multiplier: N[];
+  quotient: N[];
+};
+type PushNumbType = {
+  objData: ResultNumbersType<number>;
+  data: number;
+  path: "multiplier" | "quotient";
+};
+type StateType<N extends number> = {
+  [K: number]: ResultNumbersType<N>;
+};
 
 export const useRandomInt = ({ min, max, length }: Prop<number>) => {
   const randomInteger = (params: RandomProp<number>): number =>
     +Math.floor(params.min + Math.random() * (params.max + 1 - params.min));
 
-  type ReturnTypeRandom = ReturnType<typeof randomInteger>;
-  const resultNumbers: {
-    [key: ReturnTypeRandom]: { multiplier: number[]; quotient: number[] };
-  } = {};
+  const resultNumbers: StateType<number> = {};
 
   for (let i = 1; i <= length; i++)
     resultNumbers[randomInteger({ min, max })] = {
@@ -18,20 +27,27 @@ export const useRandomInt = ({ min, max, length }: Prop<number>) => {
 
   const keys: Array<string> = Object.keys(resultNumbers);
 
-  function pushInArr(el: number): void {
-    let quotient: number = el;
+  const pushNumberInArray = ({ objData, data, path }: PushNumbType): number =>
+    objData[path].push(data);
 
+  function getFactorizationNumber(el: number): void {
+    let quotient: number = el;
+    const initData: PushNumbType = {
+      objData: resultNumbers[el],
+      data: el,
+      path: "quotient",
+    };
+    pushNumberInArray({ ...initData });
     for (let i = 2; i <= quotient; ) {
       if (quotient % i === 0) {
-        const objData = resultNumbers[el];
-        objData.multiplier.push(i);
+        pushNumberInArray({ ...initData, data: i, path: "multiplier" });
         quotient /= i;
-        objData.quotient.push(quotient);
+        pushNumberInArray({ ...initData, data: quotient });
       } else i++;
     }
   }
 
-  keys.forEach((item) => pushInArr(+item));
+  keys.forEach((item) => getFactorizationNumber(+item));
 
   return resultNumbers;
 };
