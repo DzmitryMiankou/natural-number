6847c282a817aa6cc65b8093af89d9ae46b0823b
DzmitryMiankou <gmiankou@gmail.com>
2024-04-28 11:51:27 +0300

    resizeWindHook

--- src/hook/resizeWindHook.tsx
+++ src/hook/resizeWindHook.tsx
@@ -4,13 +4,11 @@ export const useResize = (): { width: number } => {
   const [width, setWidth] = useState<number>(window.innerWidth);
 
   useEffect(() => {
-    const handleResize = (event: UIEvent) => {
+    const handleResize = (event: UIEvent) =>
       setWidth((event.target as Window).innerWidth);
-    };
+
     window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
   return {
     width,
